9c7ddfb6c8cac46a849745c08ce57d8bb9e90465
vigneshviki19 <vigneshvengatesh07@gmail.com>
2025-12-16 10:04:53 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,11 +1,33 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  username: { type: String, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, default: "user" }, // user | admin
-  createdAt: { type: Date, default: Date.now }
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user"
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", userSchema);
