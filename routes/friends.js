e98e2ce7143312f2d69ac0f7ddf37f6d58a92539
vigneshviki19 <vigneshvengatesh07@gmail.com>
2025-12-23 18:06:50 +0530

    Update friends.js

--- routes/friends.js
+++ routes/friends.js
@@ -1,59 +1,14 @@
 const express = require("express");
+const router = express.Router();
 const User = require("../models/User");
 const auth = require("../middleware/auth");
 
-const router = express.Router();
-
-/* ➕ SEND FRIEND REQUEST */
-router.post("/request/:username", auth, async (req, res) => {
-  const fromUser = await User.findById(req.user.id);
-  const toUser = await User.findOne({ username: req.params.username });
-
-  if (!toUser) return res.status(404).json({ message: "User not found" });
-  if (toUser.friends.includes(fromUser._id))
-    return res.json({ message: "Already friends" });
-
-  if (!toUser.friendRequests.includes(fromUser._id)) {
-    toUser.friendRequests.push(fromUser._id);
-    await toUser.save();
-  }
-
-  res.json({ message: "Friend request sent" });
-});
-
-/* ✅ ACCEPT FRIEND REQUEST */
-router.post("/accept/:username", auth, async (req, res) => {
-  const user = await User.findById(req.user.id);
-  const other = await User.findOne({ username: req.params.username });
-
-  if (!other) return res.status(404).json({ message: "User not found" });
-
-  user.friendRequests = user.friendRequests.filter(
-    (id) => id.toString() !== other._id.toString()
-  );
-
-  user.friends.push(other._id);
-  other.friends.push(user._id);
-
-  await user.save();
-  await other.save();
-
-  res.json({ message: "Friend added" });
-});
-
-/* 👥 GET FRIEND LIST */
+/**
+ * GET FRIENDS LIST
+ */
 router.get("/", auth, async (req, res) => {
-  const user = await User.findById(req.user.id).populate("friends", "username");
-  res.json(user.friends);
-});
-
-/* 🔔 GET FRIEND REQUESTS */
-router.get("/requests", auth, async (req, res) => {
-  const user = await User.findById(req.user.id).populate(
-    "friendRequests",
-    "username"
-  );
-  res.json(user.friendRequests);
+  const user = await User.findById(req.user.id);
+  res.json(user.friends || []);
 });
 
 module.exports = router;
