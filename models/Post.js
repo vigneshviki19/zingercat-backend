21a2ee6b86ee1002c62ddf0219512986bce2a02d
vigneshviki19 <vigneshvengatesh07@gmail.com>
2025-12-28 22:46:32 +0530

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -2,12 +2,26 @@ const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema(
   {
-    content: { type: String, required: true },
-    author: String,
-    department: String,
-    college: String,
-    image: String,
-    likes: { type: Number, default: 0 }
+    content: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String, // image URL
+      default: ""
+    },
+    author: {
+      type: String, // username
+      required: true
+    },
+    userId: {
+      type: String,
+      required: true
+    },
+    likes: {
+      type: Number,
+      default: 0
+    }
   },
   { timestamps: true }
 );
