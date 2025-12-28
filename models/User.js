4b0500adb8d9c42f98ab6822d026e716ab65af95
vigneshviki19 <vigneshvengatesh07@gmail.com>
2025-12-28 12:44:21 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,32 +1,17 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  about: {
-    type: String,
-    default: ""
-  },
-  friends: {
-    type: [String],   // store usernames
-    default: []       // 🔥 THIS FIXES THE CRASH
-  },
-  friendRequests: {
-    type: [String],
-    default: []
-  }
+  username: { type: String, unique: true },
+  email: { type: String, unique: true },
+  password: String,
+
+  name: String,
+  department: String,
+  year: String,
+  about: String,
+
+  friends: { type: [String], default: [] },
+  friendRequests: { type: [String], default: [] }
 });
 
 module.exports = mongoose.model("User", UserSchema);
