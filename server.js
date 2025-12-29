a1030dddaf44c12cf7d2fe86543e0f1a3b58dde7
vigneshviki19 <vigneshvengatesh07@gmail.com>
2025-12-29 09:35:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,11 +32,6 @@ const io = new Server(server, {
 app.use(cors());
 app.use(express.json());
 
-// ✅ SERVE UPLOADS (IMPORTANT)
-app.use(
-  "/uploads",
-  express.static(path.join(__dirname, "uploads"))
-);
 
 // 🔥 DATABASE
 mongoose
