305266c90681277226bd023610fa1a2f8c619c43
vigneshviki19 <vigneshvengatesh07@gmail.com>
2025-12-29 13:13:16 +0530

    Create Comment.js

---
+++ models/Comment.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema(
+  {
+    postId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+      required: true
+    },
+    author: {
+      type: String,
+      required: true
+    },
+    userId: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    parentComment: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+      default: null // null = top-level comment
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Comment", commentSchema);
