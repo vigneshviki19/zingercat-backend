f446234e150444d4f9b5056478cb0f60dea4e56a
vigneshviki19 <vigneshvengatesh07@gmail.com>
2026-02-03 17:28:10 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -18,40 +18,17 @@ const UserSchema = new mongoose.Schema({
     required: true
   },
 
-  /* 🔥 PROFILE FIELDS */
-  name: {
-    type: String,
-    default: ""
-  },
+  /* PROFILE */
+  name: { type: String, default: "" },
+  dept: { type: String, default: "" },
+  startYear: { type: Number, default: 2020 },
+  endYear: { type: Number, default: 2024 },
+  about: { type: String, default: "" },
+  profilePic: { type: String, default: "" },
 
-  dept: {
-    type: String,
-    default: ""
-  },
-
-  startYear: {
-    type: Number,
-    default: 2020
-  },
-
-  endYear: {
-    type: Number,
-    default: 2024
-  },
-
-  about: {
-    type: String,
-    default: ""
-  },
-
-  profilePic: {
-    type: String,
-    default: "" // default avatar later
-  },
-
- friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  /* FRIEND SYSTEM */
+  friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   friendRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
-
 });
 
 module.exports = mongoose.model("User", UserSchema);
